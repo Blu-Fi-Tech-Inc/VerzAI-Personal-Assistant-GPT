0752a0e5073044c830f11935f96c0769a77ba46c
Blu.Fi Tech, Inc <blufitech@gmail.com>
2024-06-14 09:45:40 -0400

    Update server.js

--- server/server.js
+++ server/server.js
@@ -7,7 +7,7 @@ dotenv.config()
 
 const app = express()
 app.use(cors({
-  origin: 'http://localhost:5173'
+  origin: 'https://verzai-personal-assistant-gpt.onrender.com'
 }));
 app.use(express.json());
 
