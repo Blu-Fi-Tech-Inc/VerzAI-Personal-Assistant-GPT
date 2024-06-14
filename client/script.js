b9dd4565d8d68ab13d55cca518ce73a4cc0c89b6
Blu.Fi Tech, Inc <blufitech@gmail.com>
2024-06-14 09:39:25 -0400

    Update script.js

--- client/script.js
+++ client/script.js
@@ -76,7 +76,7 @@ const handleSubmit = async (e) => {
     loader(messageDiv);
 
     try {
-        const response = await fetch('http://localhost:5000', {
+        const response = await fetch('https://verzai-personal-assistant-gpt.onrender.com', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
